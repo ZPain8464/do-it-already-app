205f52939c55656ef788d3f037e2fd1baa11e8d1
Zach Painter <z.painter45@gmail.com>
2020-11-28 12:15:24 -0600

    edited base URL

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,6 +1,6 @@
 export default {
   PORT: process.env.PORT || 8000,
-  REACT_APP_API_BASE_URL: "https://peaceful-shore-18793.herokuapp.com/",
+  REACT_APP_API_BASE_URL: "https://peaceful-shore-18793.herokuapp.com",
   API_KEY: process.env.API_KEY,
   TOKEN_KEY: "do-it-already-token",
 };
